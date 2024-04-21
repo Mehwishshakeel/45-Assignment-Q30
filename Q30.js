f4698a5121b0344271b343cb4616c69edc623f2c
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-04-21 14:51:26 +0500

    push to github

---
+++ Q30.js
@@ -0,0 +1,12 @@
+// making an array of userName
+let userNames = ["Arsalan", "Maaz", "Areeb", "Tahir", "Admin"];
+// using forEach loop on array
+userNames.forEach(oneUser => {
+    if (oneUser === "Admin") {
+        console.log(`Hello ${oneUser}, would you like to see a status report?`);
+    }
+    else {
+        console.log(`Hello ${oneUser} ,thank you for logging in again`);
+    }
+});
+export {};
